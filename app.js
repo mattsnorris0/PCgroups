08253a17904815f5dac08050562a138131e38e4d
mattsnorris0 <145632276+mattsnorris0@users.noreply.github.com>
2025-01-11 10:56:43 -0600

    Update app.js

--- app.js
+++ app.js
@@ -15459,7 +15459,7 @@ function App() {
   const [isLoading, setIsLoading] = reactExports.useState(true);
   const fetchGroups = async () => {
     try {
-      const url = "church-web-api--soma-church-web-app.us-central1.hosted.app/groups";
+      const url = "https://church-web-api--soma-church-web-app.us-central1.hosted.app/groups";
       const res = await fetch(url, {
         method: "GET",
         headers: {
